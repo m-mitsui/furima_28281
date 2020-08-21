02920da133787c7f49fc1f5942d70e7a5e652d10
m-mitsui <paperclip321@icloud.com>
2020-08-21 20:16:11 +0900

    正規表現を変数に代入

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -7,7 +7,10 @@ class Consumer < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    
+    zenkaku_kanakanji = /\A[ぁ-んァ-ン一-龥]/
+    hankakueisuukigou = /\A\S+@\S+\.\S+\z/
+    hannkakueisuu = /\A[a-zA-Z0-9]+\z/
+    zenkakuk_kana = /\A[ァ-ヶー－]+\z/
   with_options presence: true do
     validates :nickname, format: {with: /\A[ぁ-んァ-ン一-龥]/}
     validates :email,  format: {with: /\A\S+@\S+\.\S+\z/} #/\A\S+@\S+\.\S+\z/
