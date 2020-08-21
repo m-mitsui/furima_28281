42dec6b22eb0a66f66655f604b42aefb04896f8c
m-mitsui <paperclip321@icloud.com>
2020-08-21 21:49:53 +0900

    テストコードの追加

--- spec/models/consumer_spec.rb
+++ spec/models/consumer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Consumer, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+   pending "add some examples to (or delete) #{__FILE__}"
   
   before do
     @consumer = FactoryBot.build(:consumer)
@@ -45,14 +45,48 @@ RSpec.describe Consumer, type: :model do
         @consumer.valid?
         expect(@consumer.errors.full_messages).to include("Email can't be blank", "Email can't be blank")
       end
+      
 
-      it  "passwordがありpassword_confirmationと一致していないと登録できない" do
+      it "メールアドレスが一意性でなければ登録できない" do
+        @consumer.save
+        another_consumer = FactoryBot.build(:consumer)
+        another_consumer.email = @consumer.email
+        another_consumer.valid?
+        expect(another_consumer.errors.full_messages).to include("Email has already been taken")
+      end
+
+      it "emailは@を含んでいなければ登録できない。" do
+        @consumer.email = "address123address.com"
+        @consumer.valid?
+        expect(@consumer.errors.full_messages).to include("Email is invalid")
+      end
+
+      it "passwordが空では登録できない" do
         @consumer.password = ""
         @consumer.valid?
         expect(@consumer.errors.full_messages).to include("Password can't be blank",
           "Password can't be blank",
           "Password confirmation doesn't match Password"
           )
+      end
+
+      it "password_confirmationを入力しpasswordと一致していないと登録できない" do
+        @consumer.password_confirmation = "password"
+        @consumer.valid?
+        expect(@consumer.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it "passwordが６文字以上でなければ登録できない" do
+        @consumer.password = "abc12"
+        @consumer.valid?
+        expect(@consumer.errors.full_messages).to include("Password confirmation doesn't match Password",
+          "Password is too short (minimum is 6 characters)")
+      end
+
+      it "passwordが半角英数混合でなければ登録できない" do
+        @consumer.password = "abcdef"
+        @consumer.valid?
+        expect(@consumer.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it "姓名、氏名がないと登録できない" do
